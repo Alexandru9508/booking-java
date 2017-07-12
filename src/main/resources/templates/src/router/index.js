cf47fa11623f9e874fb8cd29e8390c3d5e2a8a25
Sebastian H <sebastian.hatnean@gmail.com>
2017-07-12 15:48:15 +0300

    #add demo

--- src/main/resources/templates/src/router/index.js
+++ src/main/resources/templates/src/router/index.js
@@ -3,20 +3,34 @@ import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Home from '@/components/Home'
 import Register from '@/components/Register'
+import Register from '@/components/Register'
+import Register from '@/components/Register'
+import Register from '@/components/Register'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-  },
-    {
-      path: '/register',
-      name: 'Register',
-      component: Register
-    }
-  ]
+	routes: [
+	{
+		path: '/',
+		name: 'Home',
+		component: Home
+	},
+	{
+		path: '/register',
+		name: 'Register',
+		component: Register
+	},
+	{
+		path: '/login',
+		name: 'Register',
+		component: Register
+	},
+	{
+		path: '/bookings',
+		name: 'Register',
+		component: Register
+	}
+	]
 })
