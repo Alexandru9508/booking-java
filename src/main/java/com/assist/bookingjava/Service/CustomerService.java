fee776fe12f5634b62614d81fc043e20cd39ced4
DorofteiIonut <doroftei_a@ymail.com>
2017-07-13 13:29:36 +0300

    New model for database connection

---
+++ src/main/java/com/assist/bookingjava/Service/CustomerService.java
@@ -0,0 +1,38 @@
+package com.assist.bookingjava.Service;
+
+import com.assist.bookingjava.DataBase.CustomerDao;
+import com.assist.bookingjava.Models.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by doroftei on 13.07.2017.
+ */
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerDao customerDao;
+
+    public List<Customer> getAllCust(long id){
+        List<Customer> customersList=new ArrayList<>();
+        customersList.add(customerDao.findOne(id));
+        return customersList;
+    }
+
+    public Customer getCust(long id) {
+        return customerDao.findOne(id);
+    }
+    public void addCust(Customer customer){
+        customerDao.save(customer);
+    }
+
+    public List<Customer> getCust() {
+        List<Customer> courses = new ArrayList<>();
+         customerDao.findAll().forEach(courses :: add);
+        return courses;
+    }
+
+}
