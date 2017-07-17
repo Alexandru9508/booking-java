94dfb26454ae2278c9165b58992089e6019e1330
NMM26 <nemtoim@gmail.com>
2017-07-17 12:23:36 +0300

    eror

--- src/main/resources/templates/src/components/Bookingservices.vue
+++ src/main/resources/templates/src/components/Bookingservices.vue
@@ -2,56 +2,76 @@
     <div class="nmm">
       <nav>
         <ul class="nav-bar">
-          <li <a class="float-left" href="#about">Booking App</a>
-            <li><a href="#about">PROFILE</a></li>
-            <li><a href="#contact">CALENDAR</a></li>
-            <li><a href="#news">BOOKING</a></li>
-            <li><a href="#home">DASHBOARD</a></li>
+          <li <a class="float-left"</a>
+            <li><a href="UploadLogo.vue">PROFILE</a></li>
+            <li><a href="calendar.vue">CALENDAR</a></li>
+            <li><a href="Booking.vue">BOOKING</a></li>
+            <li><a href="Bookingservices.vue">DASHBOARD</a></li>
           </ul>
         </nav>  
-        <div class="container">
-        <div class="bar-buton">
-          <button class="btn buton" href="#">New service</button>
+        
+          <div class="bar-buton">
+            <button class="btn buton" href="#">+</button>
           </div>
+          
           <div class="nameoftheservice">
-          <div class="list>">
-            <div class="describeservices">
-              <div class="parent">
-                <h6 id="colLeft">Name of the service</h6>
-                <div id="colCenter"></div>
-                <div id="colRight"><img src="..//assets/tr.png" style="width:20px;height:20px;"> </div>
-                <div id="colRight"><img src="..//assets/pen.png" style="width:20px;height:20px;"></div>
+            <div class="list>">
+              <div class="describeservices">
+                <div class="parent">
+                  <h6 id="colLeft">Name of the service</h6>
+                  <div id="colCenter"></div>
+                  <div class="add-delete-button">
+                    <div id="colRight">
+                      <b-button class="add-button" href="">
+
+                      </b-button>
+                    </div>
+                    <div id="colRight">
+                      <b-button class="delete-button"  href="">
+
+                      </b-button>
+
+                    </div>
+                  </div>
+                </div>
+                <div class="describe">
+                <p> Disrupt jean shorts viral hella meh, plaid cupidatat magna art party. Echo Park adipisicing literally narwhal. Williamsburg leggings church-key, craft beer forage cornhole jean shorts blue bottle pariatur. Officia sapiente bespoke, locavore plaid cray voluptate deep v ex vinyl tote bag chillwave swag occaecat. Sed banh mi 3 wolf moon single-origin coffee quis tempor. Hoodie pitchfork pork belly aliqua, shabby chic elit consequat freegan ethical try-hard mixtape. Schlitz banjo deep v ullamco blog, umami nulla sint elit skateboard Godard odio.</p>
+                </div>
+                <table class="price">
+                  <tr>
+                    <th>Mon-Fri, 08:00 - 16:00</th>
+                    <th>6 available</th> 
+                    <th>hour  1hour </th>
+                    <th>210 Lei</th>
+                  </tr>
+                  <tr class="pricedescribe">
+                    <td>Avalibility</td>
+                    <td>Spaces</td> 
+                    <td>Duration</td>
+                    <td>Price</td>
+                  </tr>
+                </table>              
               </div>
-              <p> Disrupt jean shorts viral hella meh, plaid cupidatat magna art party. Echo Park adipisicing literally narwhal. Williamsburg leggings church-key, craft beer forage cornhole jean shorts blue bottle pariatur. Officia sapiente bespoke, locavore plaid cray voluptate deep v ex vinyl tote bag chillwave swag occaecat. Sed banh mi 3 wolf moon single-origin coffee quis tempor. Hoodie pitchfork pork belly aliqua, shabby chic elit consequat freegan ethical try-hard mixtape. Schlitz banjo deep v ullamco blog, umami nulla sint elit skateboard Godard odio.</p>
-              <table class="price">
-                <tr>
-                  <th>Mon-Fri, 08:00 - 16:00</th>
-                  <th>6 available</th> 
-                  <th>hour  1hour </th>
-                  <th>210 Lei</th>
-                </tr>
-                <tr class="pricedescribe">
-                  <td>Avalibility</td>
-                  <td>Spaces</td> 
-                  <td>Duration</td>
-                  <td>Price</td>
-                </tr>
-              </table>              
             </div>
           </div>
-        </div>
-      </div>
+        
       </div>
     </template>
   </script>
   <style>
 
+
+    .describe{
+      
+      margin-top: 10px;
+    }
+
     .nmm
-  {
-    font-family: 'Open Sans bold', sans-serif;
-    width: 100%;
-    margin:auto;
-  }
+    {
+      font-family: 'Open Sans bold', sans-serif;
+      width: 100%;
+      margin:auto;
+    }
 
     .float-left{
       font-size: 25px;
@@ -62,14 +82,24 @@
       text-decoration: none;
     }
 
+    .add-button{
+      width: 2.1em;  height: 2.3em;
+      text-align: middle;
+      background-size: 100%  non-reapeat ;
+      background-image: url(../assets/pen.png)
+    }
 
-
-
+    .delete-button{
+      width: 2.1em;  height: 2.3em;
+      background-size: 100% non-reapeat ;
+      background-image: url(../assets/tr.png)
+    }
 
     .nav-bar {
+      font-size: 12px ;
       list-style-type: none;
       margin: 0;
-      padding-right: 40px;
+      padding-right: 13px;
       padding-top: 20px;
       overflow: hidden;
       background-color: #8875e4;
@@ -101,12 +131,19 @@
 
     .buton {
 
-    background: #d145e9;
-    color: #fff;
-    margin-left: 64%;
-    margin-top: 20px;
-    margin-bottom: 20px;
+      font-size: 25px;
+      border-radius: 100%;
+      background: #d145e9;
+      color: #fff;
+      margin-left: 66%;
+      margin-top: 20px;
+      margin-bottom: 20px;
     }
+    .buton:hover {
+    background: #bf6cce;
+}
+
+
 
     #about
     {
@@ -115,14 +152,14 @@
 
     .parent
     {
-      
+
       width: 100%;
       height: 40px;
       padding-top: 10px;
       margin: auto; 
       border-radius: 2px;
       color:rgb(209, 69, 233);
-      
+
     }
 
     .list
@@ -133,45 +170,46 @@
     table
 
     .nameoftheservice {
-    margin: auto;
-    width: 600px;
-    background-color: white;
-    color:#d145e9;
-  }
+      margin: auto;
+      width: 500px;
+      background-color: white;
+      color:#d145e9;
+    }
 
-  .price {
-    width:100%;
-    color: black;
-    border:0;
-    font-size: 12px;
-  }
+    .price {
+      width:100%;
+      color: #565656;
+      border:0;
+      font-size: 12px;
+    }
 
-  .price th {
-    border:none;
-  }
+    .price th {
+      border:none;
+    }
 
-  .price td {
-    border-top: none;
-  }
+    .price td {
+      border-top: none;
+    }
 
-  .pricedescribe {
-    color: gray;
-    font-size: 10px;
-  }
+    .pricedescribe {
+      color: gray;
+      font-size: 10px;
+    }
 
-  .describeservices {
-    width: 50%;
-    background-color: white;
-    margin: auto;
-    border-radius: 2px;
-    text-align: left;
-    color: #929292;
-    border: 0px;
-    border-color: gray;
-    box-shadow:  0 0 0.2em #8875e4;
-    padding-left: 20px;
-    padding-right: 20px;
-  }
+    .describeservices {
+      width: 40%;
+      background-color: white;
+      margin: auto;
+      border-radius: 2px;
+      text-align: left;
+      color: #929292;
+      border: 0px;
+      border-color: gray;
+      box-shadow:  0 0 1em rgba(209, 69, 233, 0.37);
+      padding-left: 20px;
+      padding-right: 20px;
+      padding-bottom: 20px;
+    }
 
     /*------------col*/
 
@@ -209,12 +247,12 @@
 
   </style>
   <script>
-	export default {
-	  servicename: 'bookingservices',
-	  data () {
-	    return {
-	      msg: 'Welcome to Your Vue.js App'
-	    }
-	  }
-	}
-</script>
+    export default {
+      servicename: 'bookingservices',
+      data () {
+        return {
+          msg: 'Welcome to Your Vue.js App'
+        }
+      }
+    }
+  </script>
--- src/main/resources/templates/src/components/Home.vue
+++ src/main/resources/templates/src/components/Home.vue
@@ -1,8 +1,14 @@
 <template>
 	<div>
-    	<router-link to="addService">AddService</router-link>
+    	<router-link to="AddService">AddService</router-link>
     	<router-link to="UploadLogo">UploadLogo</router-link>
     	<router-link to="BookingPersonDetails">BookingPersonDetails</router-link>
+    	<router-link to="Booking">Booking</router-link>
+    	<router-link to="Bookingservices">Bookingservices</router-link>
+    	<router-link to="Companyname">Companyname</router-link>
+    	<router-link to="Recover">Recover</router-link>
+    	<router-link to="Register">Register</router-link>
+    	<router-link to="Login">Login</router-link>
     	<p>asdasda</p>
 	</div>
 
