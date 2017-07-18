a36ea43d51ef6c33c86182a8dd3efa62262a7c5e
Gherasim Paul <intern.paul.gherasim@assist.ro>
2017-07-18 04:59:30 -0400

    logare

--- src/main/resources/templates/src/components/Login.vue
+++ src/main/resources/templates/src/components/Login.vue
@@ -9,7 +9,7 @@
 	  	  		<!-- email -->
 	    		<form> 
 		       		<div class="column is-12">
-    			<label class="label" for="email">Email address:</label>
+    			<label class="labelEmail" for="email">Email address:</label>
     			<p :class="{ 'control': true }">
         		<input v-model="user.email" v-validate="'required|email'" :class="{'input': true, 'is-danger': errors.has('email') }" name="email" type="text" placeholder="" class="inputEmail">
         		</p>
@@ -26,9 +26,9 @@
 		     		<input type="password" class="passwordLogin" name="user_password"></br>
  -->
  				<div class="column is-12">
-            <label class="label">Password</label>
+            <label class="labelPassword">Password</label>
             <p class="control has-icon has-icon-right">
-                <input v-model="user.password" v-validate="'required|min:6'" :class="{'input': true, 'is-danger': errors.has('password') }" type="password" placeholder="" class="inputEmail">
+                <input v-model="user.password" v-validate="'required|min:6'" :class="{'input': true, 'is-danger': errors.has('password') }" name="password" type="password" placeholder="" class="inputEmail">
                 <p>
                 <p>
                 <i v-show="errors.has('password')" class="fa fa-warning"></i>
@@ -50,66 +50,6 @@
 	  </div>
   </div>
 </template>
-
-
-<style type="text/css">
-p{
-	color:#990000;
-}
-.passwordLogin {
-	font-size: 100%;
-	width: 28%;
-}
-.inputEmail {
-	font-size: 180%;
-	width: 28%;
-}
-#submit{
-	margin-top: 5%;
-	font-size: 190%;
-	width: 28%;
-	background-color:#DA6CE9;
-	border:0px;
-	color:#ffffff;
-
-}
-.all{
-	text-align: center;
-}
-.image {
-	
-	margin-top:8.2%;
-	margin-bottom: 4%;
-
-
-}
-
-.label{
-	margin-top: 2%;
-	margin-left: -20.5%;
-	width: 20%
-}
-.elementPassword{
-
-	margin-top: 2%;
-	margin-left: -20.6%;
-	width: 20%
-}
-.goToRecover{
-	margin-top: 2%;
-}
-
-
-/*Responsive*/
-
-@media (max-width: 1000px) {
-	.form-group {
-		float: none;
-		margin-right: 0;
-	}
-}
-</style>
-
 <script>
 	import axios from 'axios';
 	export default {
@@ -140,3 +80,64 @@ p{
 
 </script>
 
+<style type="text/css">
+p{
+	color:#990000;
+}
+.passwordLogin {
+	font-size: 100%;
+	width: 28%;
+}
+.inputEmail {
+	font-size: 180%;
+	width: 28%;
+}
+#submit{
+	margin-top: 5%;
+	font-size: 190%;
+	width: 28%;
+	background-color:#DA6CE9;
+	border:0px;
+	color:#ffffff;
+
+}
+.all{
+	text-align: center;
+}
+.image {
+	
+	margin-top:8.2%;
+	margin-bottom: 4%;
+}
+
+.labelPassword{
+	margin-top: 2%;
+	margin-left: -20.5%;
+	width: 20%
+
+}.labelEmail{
+	margin-top: 2%;
+	margin-left: -19%;
+	width: 20%
+}
+
+.elementPassword{
+
+	margin-top: 2%;
+	margin-left: -20.6%;
+	width: 20%
+}
+.goToRecover{
+	margin-top: 2%;
+}
+
+
+/*Responsive*/
+
+@media (max-width: 1000px) {
+	.form-group {
+		float: none;
+		margin-right: 0;
+	}
+}
+</style>
