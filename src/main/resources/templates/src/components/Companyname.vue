2355c75ea4757404111bb4627e2e48f4a6b58767
NMM26 <nemtoim@gmail.com>
2017-07-19 17:18:51 +0300

    services info per company

--- src/main/resources/templates/src/components/Companyname.vue
+++ src/main/resources/templates/src/components/Companyname.vue
@@ -5,160 +5,136 @@
 
 <template>
 	<div class="nmm">
+	<nav-bar></nav-bar>
 		<div class="container">
+
 			<div class="companyname">
 				<div class="logo">
 					<img src="..//assets/l2.png" style="width:150px;height:150px;">
 				</div>
 				<div class="parent">
-					<h6 id="colLeft">Company name</h6>             
+					<h6 id="colLeft">{{currentCompany.companyname}}</h6>             
 				</div>
 				<div class="describeservices2">                
-					<p> Disrupt jean shorts viral hella meh, plaid cupidatat magna art party. Echo Park adipisicing literally narwhal. Williamsburg leggings church-key, craft beer forage cornhole jean shorts blue bottle pariatur. Officia sapiente bespoke, locavore plaid cray voluptate deep v ex vinyl tote bag chillwave swag occaecat. Sed banh mi 3 wolf moon single-origin coffee quis tempor. Hoodie pitchfork pork belly aliqua, shabby chic elit consequat freegan ethical try-hard mixtape. Schlitz banjo deep v ullamco blog, umami nulla sint elit skateboard Godard odio.</p>            
+					<p> {{currentCompany.description}}</p>            
 				</div>           
 			</div>
-			<div class="background-bottom">
-				<div class="nameoftheservice">
-					<div class="list>">
-						<div class="describeservices">
-							<div class="parent">
-								<h6 id="colLeft">Name of the service</h6>
-								<div id="colCenter"></div>
-								<div class="add-delete-button">
-									<div id="colRight">
-										<b-button class="add-button" href="">
+			<div v-for="company in companies.body"> 
+				<div class="background-bottom">
+					<div class="nameoftheservice">
+						<div class="list>">
+							<div class="describeservices">
+								<div class="parent">
+									<h6 id="colLeft">
 
-										</b-button>
-									</div>
-									<div id="colRight">
-										<b-button class="delete-button"  href="">
 
-										</b-button>
-
+									{{company.servicename}}</h6>
+									<div id="colCenter"></div>
+									<div class="add-delete-button">
+										<div id="colRight">
+											<b-button class="add-button" href="">
+											</b-button>
+										</div>
+										<div id="colRight">
+											<b-button class="delete-button"  href="">
+											</b-button>
+										</div>
 									</div>
 								</div>
+								<p> {{company.servicename}}</p>
+								<table class="price">
+									<tr>
+										<th>Mon-Fri, 08:00 - 16:00</th>
+										<th>{{company.space}} available</th> 
+										<th>{{company.duration}} hour </th>
+										<th>{{company.price}} Lei</th>
+									</tr>
+									<tr class="pricedescribe">
+										<td>Avalibility</td>
+										<td>Spaces</td> 
+										<td>Duration</td>
+										<td>Price</td>
+									</tr>
+								</table>
+								<b-button class="butonnewservices2" @click="showModal" ref="btnShow">
+									BOOK NOW
+								</b-button>
 							</div>
-							<p> Disrupt jean shorts viral hella meh, plaid cupidatat magna art party. Echo Park adipisicing literally narwhal. Williamsburg leggings church-key, craft beer forage cornhole jean shorts blue bottle pariatur. Officia sapiente bespoke, locavore plaid cray voluptate deep v ex vinyl tote bag chillwave swag occaecat. Sed banh mi 3 wolf moon single-origin coffee quis tempor. Hoodie pitchfork pork belly aliqua, shabby chic elit consequat freegan ethical try-hard mixtape. Schlitz banjo deep v ullamco blog, umami nulla sint elit skateboard Godard odio.</p>
-							<table class="price">
-								<tr>
-									<th>Mon-Fri, 08:00 - 16:00</th>
-									<th>6 available</th> 
-									<th>hour  1hour </th>
-									<th>210 Lei</th>
-								</tr>
-								<tr class="pricedescribe">
-									<td>Avalibility</td>
-									<td>Spaces</td> 
-									<td>Duration</td>
-									<td>Price</td>
-								</tr>
-							</table>
-
-
-							<b-button class="butonnewservices2" @click="showModal" ref="btnShow">
-								BOOK NOW
-							</b-button>
-
-
-
 							<b-modal id="modal1">
-
-								 <div>
-
+								<div>
 									<div class="container-fluid">
-
-
 										<div class="row">
 											<div class="form_container clearfix">
 												<form class=" serviceForm">
-												<div v-show="step1">
-													<h3 id="titleService" >Make your booking</h3>	
-													<h6 id="personaldetalis">PERSONAL DETAILS</h6>			
-
-
-													<div class="form-group1">							
-														<label for="name"><h5>Name</h5></label>
-														<input type="text" class="form-control box-size" required>								
+													<div v-show="step1">
+														<h3 id="titleService" >Make your booking</h3>	
+														<h6 id="personaldetalis">PERSONAL DETAILS</h6>
+														<div class="form-group1">							
+															<label for="name"><h5>Name</h5></label>
+															<input type="text" class="form-control box-size" required>								
+														</div>
+														<div class="form-group2">
+															<label for="email"><h5>Email</h5></label>
+															<input type="text" class="form-control box-size" required>
+														</div>
+														<div class="form-group3">
+															<label for="phonenumber"><h5>Phone Number</h5></label>
+															<input type="text" class="form-control box-size" required>
+														</div>
+														<div class="form-group4">
+															<span id="goToStep2" @click="goToStep('step2', 'step1')" class="form-control btn btn-next">NEXT</span>			
+														</div>	
 													</div>
-
-													<div class="form-group2">
-														<label for="email"><h5>Email</h5></label>
-														<input type="text" class="form-control box-size" required>
-													</div>
-
-													<div class="form-group3">
-
-														<label for="phonenumber"><h5>Phone Number</h5></label>
-														<input type="text" class="form-control box-size" required>
-													</div>
-
-													<div class="form-group4">
-														<span id="goToStep2" @click="goToStep('step2', 'step1')" class="form-control btn btn-next">NEXT</span>						
-													</div>	
-												</div>
-
 													<div v-show="step2">
 														<h2>step 2</h2>
-														<span id="goToStep1"  @click="goToStep('step1', 'step2')" class="form-control btn btn-next">Back</span>						
+														<span id="goToStep1"  @click="goToStep('step1', 'step2')" class="form-control btn btn-next">Back</span>			
 														<button type="submit" class="form-control btn btn-next">Submit</button>						
 													</div>
-
-
-													
 												</form>	
-
-
-						
-
 											</div>
 										</div>	
 									</div>
 								</div> 
-
 							</b-modal>
-
-<b-modal id="modalsuccces">
-													
-														<div class="imgmodal"><img src="..//assets/msg.png" style="width:35%;height:17%;"></div>
-														<div class="msgsent">
-															<h5>Your request was sent!</h5> 
-														</div>
-														<div class="describemodal">
-															In a couple of seconds a confirmation email will be sent to your email address with all the detalis for your reservation. Thank your for using our services.
-														</div>
-											
+							<b-modal id="modalsuccces">
+								<div class="imgmodal"><img src="..//assets/msg.png" style="width:35%;height:17%;"></div>
+								<div class="msgsent">
+									<h5>Your request was sent!</h5> 
+								</div>
+								<div class="describemodal">
+									In a couple of seconds a confirmation email will be sent to your email address with all the detalis for your reservation. Thank your for using our services.
+								</div>
 							</b-modal>
-
-
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- MODAL SEND MSG -->
-
-							
 	</div>
-
-
-
 </template>
-
 <script>
+
+import NavBar from './navbar.vue';
 	export default {
 		data() {
 			return {
+				companyId: '',
 				step1: true,
 				step2: false,
+				companies: [],
+				currentCompany: {}
 			}
 		},
+		components: {
+          NavBar,
+        },
 		methods: {
 			showModal() {
 				this.$root.$emit('show::modal','modal1');
 			},
 			onSubmit: function() {
 				console.log('sdadwq');
-				//afisezi modal 3 de succes
+			//afisezi modal 3 de succes
 			},
 			goToStep: function(showStep, hidestep) {
 				this[showStep] = true;
@@ -167,31 +143,26 @@
 			},
 			hideModal() {
 				this.$root.$emit('hide::modal','modal1');
-				// Return focus to our Open Modal button
-				// See accessibility below for additional return-focus methods
 				this.$refs.btnShow.$el.focus();
 			},
-			getServices(){
-				this.$http.get('http://192.168.150.242:8080/').then(response => {
-				    console.log('response success:', response);
-				  }, response => {
-				    console.log('response error: ', response);
-				  });
+			getServices() {
+				this.$http.get('http://192.168.150.242:9000/company/' + this.companyId + '/services')
+				.then(response => {
+					this.companies = response;
+					this.currentCompany = response.body[0].idcompany;
+
+				})
+				.then(error => {
+					console.log('error: ', error);
+				});
 			}
 		},
 		created(){
+			this.companyId = this.$route.params.id;
 			this.getServices();
 		}
 	}
 </script>
-
-
-	
-
-
-
-
-
 
 <style>
 	.container{
@@ -235,9 +206,6 @@
 		box-shadow:  0 0 1em rgba(209, 69, 233, 0.37);
 		padding-left: 20px;
 	}
-
-
-
 	/*----------------Bottom*/
 
 	.background-bottom {
@@ -270,7 +238,6 @@
 		font-size: 10px;
 	}
 
-
 	.add-button{
 		width: 2.1em;  height: 2.3em;
 		text-align: middle;
@@ -294,11 +261,9 @@
 		padding-top: 5%;
 	}
 
-	.describemodal
-	{
+	.describemodal{
 		text-align: center;
 		padding-top: 5%;
-
 	}
 
 	.imgmodal{
@@ -399,7 +364,6 @@
 	.form-group4{
 		float: right;
 		width: 46%;
-
 	}
 
 	.btn-next{
