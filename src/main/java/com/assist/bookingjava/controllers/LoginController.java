3445f4b845284a4d09951060c8f7c1694486197f
DorofteiIonut <doroftei_a@ymail.com>
2017-07-19 09:23:54 +0300

    Changes made on LoginController

--- src/main/java/com/assist/bookingjava/controllers/LoginController.java
+++ src/main/java/com/assist/bookingjava/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.assist.bookingjava.controllers;
 import com.assist.bookingjava.Models.Company;
 import com.assist.bookingjava.Service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +16,12 @@ public class LoginController {
     @Autowired
     CompanyService companyService;
 
+
     @RequestMapping(value = "/login",method = RequestMethod.POST)
-    public Company login(String email, String password){
-        Company company;
-        try {
-
-          company  = companyService.login(email, password);
-
-        }catch (Exception err){
-
-            return new Company();
-        }
-        return company;
-
+    public String log(@RequestBody Company company){
+       if(companyService.log(company).getPassword().equals(company.getPassword()))
+           return companyService.log(company).getEmail();
+       else
+           return "Wrong Email/Password";
     }
 }
