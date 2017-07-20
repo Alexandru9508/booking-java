f6c7611af6ed39876870eae55854773366e9f024
DorofteiIonut <doroftei_a@ymail.com>
2017-07-20 11:41:15 +0300

    Autentication methode

--- src/main/java/com/assist/bookingjava/Boot.java
+++ src/main/java/com/assist/bookingjava/Boot.java
@@ -1,19 +1,68 @@
 package com.assist.bookingjava;
 
+import com.assist.bookingjava.Models.Company;
+import com.assist.bookingjava.Service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-/**
- * Created by frunza on 07.07.2017.
- */
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Boot {
-
-
     public static void main(String[] args) {
         SpringApplication.run(Boot.class, args);
     }
-
 }
 
+@Configuration
+class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+
+    @Autowired
+    CompanyService companyService;
+
+    @Override
+    public void init(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        return email -> {
+            Company company = companyService.findByEmail(email);
+
+            if (company != null) {
+                return new User(company.getUsername(), company.getPassword(), true, true, true, true,
+                        AuthorityUtils.createAuthorityList("USER"));
+            } else {
+                throw new UsernameNotFoundException("could not find the user '"
+                        + email + "'");
+            }
+        };
+    }
+}
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+                httpBasic().and().
+                csrf().disable();
+    }
+}
